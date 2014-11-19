109047af27edcd0ce1c10558b47eb8479c72639f
Felipe Bueno <bueno.felipe@gmail.com>
2014-11-19 16:58:08 -0200

    turn on rainbow-delimiters mode after init

--- emacs/.emacs.d/lisp/setup-hooks.el
+++ emacs/.emacs.d/lisp/setup-hooks.el
@@ -2,5 +2,6 @@
 (add-hook 'after-init-hook 'global-undo-tree-mode)
 (add-hook 'after-init-hook 'yas-global-mode)
 (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook #'rainbow-delimiters-mode)
 
 (provide 'setup-hooks)
