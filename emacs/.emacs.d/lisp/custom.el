5e821ac8d04915def1381c2c62fbb030c607fa6d
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-06 11:32:57 -0200

    minor

--- emacs/.emacs.d/lisp/custom.el
+++ emacs/.emacs.d/lisp/custom.el
@@ -21,6 +21,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 180 :width normal)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 180 :width normal))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#d3d3d3"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "##8b0000")))))
 
 (provide 'custom)
