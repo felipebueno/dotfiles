ddd1f496afab8dc8bb0503a133e915a838a8e2fd
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-13 19:53:18 -0200

    emacs: add rainbow identifiers hook

--- emacs/.emacs.d/lisp/setup-hooks.el
+++ emacs/.emacs.d/lisp/setup-hooks.el
@@ -2,6 +2,7 @@
 (add-hook 'after-init-hook 'global-undo-tree-mode)
 (add-hook 'after-init-hook 'yas-global-mode)
 (add-hook 'after-init-hook 'global-company-mode)
-(add-hook 'after-init-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook  'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook  'rainbow-identifiers-mode)
 
 (provide 'setup-hooks)
