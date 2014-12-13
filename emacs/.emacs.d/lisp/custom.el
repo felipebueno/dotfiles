18b20575d91e57e588ae3d994983a2fc7360f37b
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-13 19:34:45 -0200

    minor

--- emacs/.emacs.d/lisp/custom.el
+++ emacs/.emacs.d/lisp/custom.el
@@ -6,8 +6,10 @@
  '(android-mode-avd "default")
  '(android-mode-sdk-dir "~/devel/android/sdk")
  '(company-idle-delay 0.3)
- '(custom-safe-themes (quote ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "5b3bd478f014d1ff16e1f8ee6e13329c274dd33721f14459d0d2d8f6d93f629d" "8ac31e1bc1920b33d478dfafb0b45989a00ede15a2388ea16093e7d0988c48d0" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "968d1ad07c38d02d2e5debffc5638332696ac41af7974ade6f95841359ed73e3" default)))
  '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "5b3bd478f014d1ff16e1f8ee6e13329c274dd33721f14459d0d2d8f6d93f629d" "8ac31e1bc1920b33d478dfafb0b45989a00ede15a2388ea16093e7d0988c48d0" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "968d1ad07c38d02d2e5debffc5638332696ac41af7974ade6f95841359ed73e3" default)))
  '(dired-use-ls-dired t)
  '(display-time-24hr-format t)
  '(display-time-mode t)
