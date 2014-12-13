38db33b7601620f60ad96a9b4078f5638bb0d909
Felipe Bueno <bueno.felipe@gmail.com>
2014-12-13 19:57:35 -0200

    emacs: prettier scratch buffer

--- emacs/.emacs.d/lisp/setup-scratch-buffer.el
+++ emacs/.emacs.d/lisp/setup-scratch-buffer.el
@@ -1,103 +1,18 @@
-;; Scratch buffer goodness
-;;
-;; (setq initial-scratch-message "")
-;;; I initialize my *scratch* buffer with a random Emacs haiku drawn
-;;; from among these:
-(defvar ted-emacs-haiku
-  '("Oort is so awesome
-     deuglifies Outlook crap
-     `W k' rocks"
-    "Great clouds overhead
-     Tiny black birds rise and fall
-     Snow covers Emacs
-         -- Alex Schroeder"
-    "hacking on Smyrno
-     `error in process filter'
-     something is b0rken"
-    "Swiftly typing. Oh!
-     Where would we be without you,
-     `self-insert-command'?"
-    "treeless quiet field
-     sudden bud: EmacsWiki
-     now he{ar,re} the birds sing
-         -- ttn"
-    "an emacs user's
-     fingers dance on the keyboard;
-     a nerd pianist
-         -- Erik Bourget"
-    "The file was open.
-     flying in a sparrow stole
-     a parenthesis
-         -- Oliver Scholz"
-    "The day went away.
-     The file still puts its weight on
-     the tired mode-line.
-         -- Oliver Scholz"
-    "On a cloudy day
-     you hear the cons cells whisper:
-     'We are lost and gone.'
-         -- Oliver Scholz"
-    "A message, a string
-     remind me of my sweet love.
-     Good bye, my buffers.
-         -- Oliver Scholz"
-    "Hot night in summer:
-     Hush, you quibbling characters!
-     Do not wake her up!
-         -- Oliver Scholz"
-    "A bright, busy day.
-     The windows watch a thousand
-     wild cursors dancing.
-         -- Oliver Scholz"
-    "Oh, why don't you are
-     a lake, a stream, a meadow
-     this morning, Emacs?
-         -- Oliver Scholz" ;%
-    "The friends chat gaily,
-     I stand up to join their talk.
-     My `save-excursion'.
-         -- Oliver Scholz")
-  "Haiku taken from the Emacs Wiki's EmacsHaiku page.")
+(require 'paredit)
 
-(defun ted-random-emacs-haiku (&optional prefix)
-  "Select and format a random haiku from `ted-emacs-haiku'."
-  (random t)
-  (let* ((prefix (or prefix ";; "))
-         (n (random (length ted-emacs-haiku)))
-         (haiku (nth n ted-emacs-haiku)))
-    (with-temp-buffer
-      (insert haiku)
-      (goto-char (point-min))
-      (while (< (point) (point-max))
-        (goto-char (point-at-bol))
-        (delete-horizontal-space)
-        (insert prefix)
-        (when (looking-at "--")
-          (insert "    "))
-        (forward-line 1))
-      (concat (buffer-substring-no-properties (point-min) (point-max))
-              "\n\n"))))
-(setq initial-scratch-message (ted-random-emacs-haiku))
+(defun fill-line-with-parens (x)
+  "Fill the first line of Scratch buffer with colorful parens :)"
+  (interactive)
+  (let ((count 0)
+        (dude 1))
+    (while (< count (/ (window-width) x))
+      (paredit-open-square)
+      (setq count (1+ count)))
+    (move-end-of-line nil)
+    (setq fill-column (window-width))
+    (center-line)
+    (insert "\n")))
 
-;; If the *scratch* buffer is killed, recreate it automatically
-(save-excursion
-  (set-buffer (get-buffer-create "*scratch*"))
-  (lisp-interaction-mode)
-  (make-local-variable 'kill-buffer-query-functions)
-  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer))
-
-(defun kill-scratch-buffer ()
-  ;; The next line is just in case someone calls this manually
-  (set-buffer (get-buffer-create "*scratch*"))
-  ;; Kill the current (*scratch*) buffer
-  (remove-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
-  (kill-buffer (current-buffer))
-  ;; Make a brand new *scratch* buffer
-  (set-buffer (get-buffer-create "*scratch*"))
-  (lisp-interaction-mode)
-  (make-local-variable 'kill-buffer-query-functions)
-  (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
-  ;; Since we killed it, don't let caller do that.
-  nil)
+(setq initial-scratch-message (fill-line-with-parens 2.2))
 
 (provide 'setup-scratch-buffer)
