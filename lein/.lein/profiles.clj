db75855e0f937e20764bae6536ececde5fe03b24
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-03 20:11:28 -0300

    lein: add plugins to profiles.clj

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,2 +1,9 @@
-;;
+;;{:user {:plugins []}}
+
+{:user {:plugins [
+                  ;;[refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [org.clojure/tools.nrepl "0.2.10"]
+                  ]
+        :dependencies []}}
 
