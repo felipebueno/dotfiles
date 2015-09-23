de04a45596c1a2429144fd6f7acb5f1fec36a126
Felipe Bueno <bueno.felipe@gmail.com>
2015-09-22 22:53:53 -0300

    .lein: back to stable releases

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,8 +1,8 @@
 ;;{:user {:plugins []}}
 
 {:user {:plugins [
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                  [refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.9.1"]
                   [org.clojure/tools.nrepl "0.2.10"]
                   ]
         :dependencies []}}
