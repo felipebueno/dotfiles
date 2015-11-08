ff9247ff637c61c88b6c77ccae66385af9fe9091
Felipe Bueno <bueno.felipe@gmail.com>
2015-11-07 22:31:44 -0200

    emacs: temp web-mode and flycheck config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -32,10 +32,58 @@
 
 (require 'setup-scratch-buffer)
 
+
+
+
+
+
+
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(json-jsonlist)))
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+
+
+
+
+
+
+
+
 (setq custom-file "~/.emacs.d/lisp/custom.el")
 (if (file-exists-p custom-file)
     (load-file custom-file))
 
 (require 'setup-colors-and-themes)
 
-(toggle-frame-fullscreen)
+;;(toggle-frame-fullscreen)
+
