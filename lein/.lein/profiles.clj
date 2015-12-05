f8343cad2c533c10f220d501ed2d775264c74704
Felipe Bueno <bueno.felipe@gmail.com>
2015-12-05 17:02:25 -0200

    profiles.clj: upgrade nrepl stuff :arrow_up:

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,9 +1,4 @@
-;;{:user {:plugins []}}
-
-{:user {:plugins [
-                  [refactor-nrepl "1.1.0"]
-                  [cider/cider-nrepl "0.9.1"]
-                  [org.clojure/tools.nrepl "0.2.10"]
-                  ]
-        :dependencies []}}
+{:repl {:plugins [[refactor-nrepl "1.1.0"]
+                  [cider/cider-nrepl "0.10.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
 
