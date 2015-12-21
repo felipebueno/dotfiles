ee03fde5d83f41e9e5bf3341d0ad3d934cee5f64
Felipe Bueno <bueno.felipe@gmail.com>
2015-12-21 17:51:13 -0200

    back to MELPA unstable

--- emacs/.emacs.d/lisp/setup-packages.el
+++ emacs/.emacs.d/lisp/setup-packages.el
@@ -1,10 +1,10 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
 ;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/"))
+;;              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
 (package-initialize)
 (when (not package-archive-contents)
@@ -12,10 +12,12 @@
 
 (defvar my-packages '(better-defaults
                       cider
+                      cider-eval-sexp-fu
                       clj-refactor ;; https://github.com/clojure-emacs/clj-refactor.el/issues/184#issuecomment-130728613
                       clojure-mode
                       clojure-mode-extra-font-locking
                       clojure-snippets
+                      clojure-cheatsheet
                       company
                       cyberpunk-theme
                       dockerfile-mode
@@ -24,16 +26,19 @@
                       flycheck
                       flycheck-clojure
                       flycheck-color-mode-line
+                      gradle-mode
                       idle-highlight-mode
                       ido-ubiquitous
                       js2-mode
-                      ;;json-mode
+                      json-mode
                       magit
                       markdown-mode
                       multiple-cursors
+                      nrepl-eval-sexp-fu
                       projectile
                       rainbow-delimiters
                       react-snippets
+                      shell-toggle
                       smex
                       solarized-theme
                       starter-kit
