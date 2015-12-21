f7bfd7e0445ff688243bd4486f45984bf18104c1
Felipe Bueno <bueno.felipe@gmail.com>
2015-12-21 17:52:54 -0200

    bleeding edge cider, cider-nrepl and refactor-nrepl

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,3 @@
-{:repl {:plugins [[refactor-nrepl "1.1.0"]
-                  [cider/cider-nrepl "0.10.0"]]
+{:repl {:plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
-
