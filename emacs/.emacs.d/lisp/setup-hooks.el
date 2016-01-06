83a164f923960b5806874118b0eceeac7c05c39e
Felipe Bueno <bueno.felipe@gmail.com>
2016-01-06 11:29:46 -0200

    trying company-statistics

--- emacs/.emacs.d/lisp/setup-hooks.el
+++ emacs/.emacs.d/lisp/setup-hooks.el
@@ -1,6 +1,7 @@
 (add-hook 'after-init-hook 'global-undo-tree-mode)
 (add-hook 'after-init-hook '(lambda () (yas-global-mode 1)))
 (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'company-statistics-mode)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide 'setup-hooks)
