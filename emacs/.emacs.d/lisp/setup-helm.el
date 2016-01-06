f9dc9b0427c4d0ab5035a77242551b1ac7ed8821
Felipe Bueno <bueno.felipe@gmail.com>
2016-01-06 11:30:16 -0200

    trying to use helm again

--- emacs/.emacs.d/lisp/setup-helm.el
+++ emacs/.emacs.d/lisp/setup-helm.el
@@ -1,7 +1,8 @@
-;;(require 'helm-config)
-;;(helm-autoresize-mode 1)
-;; (global-set-key (kbd "M-x") 'helm-M-x)
-;; (global-set-key (kbd "C-x C-f") 'helm-find-files)
-;; (global-set-key (kbd "C-x b") 'helm-buffers-list)
+(require 'helm)
+(require 'helm-config)
+(helm-autoresize-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 (provide 'setup-helm)
